8d2c4483389735ea8d8e49c57179a2577ace8c7d
Asdzmvc <1yulchievasadbek0071@gmail.com>
2023-12-21 12:12:29 +0500

    3xil functiondan 2tasi yozib to'ldirildi

--- clock.js
+++ clock.js
@@ -354,4 +354,24 @@
 
 //  Function expression
 
+// function evenOrOddExp(number) {
+//         if (number % 2 === 0) {
+//             console.log(`${number} is even`);
+//         } else {
+//             console.log(`${number} is odd`);
+//         }
+//     }
+//     evenOrOddExp(3002)
+
+
+// Arrow function
+
+// function evenOrOddArrow(number) {
+//     if (number % 2 === 0) {
+//         console.log(`${number} is even`);
+//     } else {
+//         console.log(`${number} is odd`);
+//     }
+// }
+// evenOrOddArrow(3)
 
