bae3f897081be7cfb25b6354e044f05ac1717f52
Asdzmvc <1yulchievasadbek0071@gmail.com>
2023-12-26 08:54:12 +0500

    alert function

--- clock.js
+++ clock.js
@@ -470,3 +470,14 @@
    // constan.push('audi');
    // console.log(constan)
 
+   function ask(question, yes, no) {
+      if (confirm(question)) yes();
+      else no();
+    }
+    
+    ask(
+      "Do you agree?",
+      function() { alert("You agreed."); },
+      function() { alert("You canceled the execution."); }
+    );
+
